c3df447dc40849542d07a563c8ba84f5b0ee57f6
mikap90 <46347480+mikap90@users.noreply.github.com>
2026-01-13 13:30:27 +0200

    Add init, ask_number, main_game_loop

--- README.py
+++ README.py
@@ -7,19 +7,38 @@
 class NumberGuessingGame:
 
     def __init__(self):
-        pass  
+        self.count = 0
        
     def generate_random_number(self):
         pass 
       
     def ask_number(self):
-        pass 
+        while True:
+            try:
+                number = int(input("Arvaa numero väliltä 1-10"))
+            except:
+                continue
+            if number < 1:
+                print("Numero oli liian pieni.")
+                continue
+            elif number > 10:
+                print("Numero on liian pieni")
+                continue
+            else:
+                break
+        return number
               
     def add_guess_count(self):
         pass  
            
     def main_game_loop(self):
-        pass
+        random_number = self.add_guess_count()
+        while True:
+            number_user = self.ask_number()
+            if number_user == random_number:
+                print(f"Oikein. Arvasit {self.count} kertaa.")
+                break
+
 
 game = NumberGuessingGame()
 
